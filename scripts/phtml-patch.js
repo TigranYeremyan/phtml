9332f3b6c521258c22bca98eca9771dc0761abc9
Jonathan Neal <jonathantneal@hotmail.com>
2019-05-07 10:29:11 -0400

    gh-pages

---
+++ scripts/phtml-patch.js
@@ -0,0 +1,56 @@
+const { Element, Plugin } = require('phtml');
+
+module.exports = new Plugin('phtml-patch', () => ({
+	HeadElement ($head) {
+		$head.append(
+			new Element({
+				name: 'link',
+				attrs: {
+					rel: 'shortcut icon',
+					type: 'image/x-icon',
+					href: 'https://phtml.io/favicon.ico'
+				},
+				isVoid: true
+			})
+		);
+	},
+	AElement ($a) {
+		if (
+			$a.attrs.contains({ href: 'index.html' }) &&
+			$a.parent.name === 'h2' &&
+			$a.nodes[0]
+		) {
+			$a.attrs.add({ href: './' });
+			$a.nodes[0].data = 'pHTML';
+		} else if (
+			$a.attrs.contains({ href: 'https://github.com/phtmlorg/phtml' }) &&
+			$a.parent.name === 'h2'
+		) {
+			const $linkBody = [
+				new Element({
+					name: 'h3',
+					nodes: [
+						'Links'
+					]
+				}),
+				new Element({
+					name: 'ul',
+					nodes: [
+						new Element({
+							name: 'li',
+							nodes: [
+								$a.clone(null, true)
+							]
+						})
+					]
+				})
+			];
+
+			const $nav = $a.parent.parent;
+
+			$a.remove();
+
+			$nav.append(...$linkBody);
+		}
+	}
+}));
